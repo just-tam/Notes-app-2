833273181db54aade72d69f9c33fc64f19e8b799
imogenkraak <imogen.kraak@gmail.com>
2020-09-15 15:34:42 +0100

    working on controller

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,9 @@
 document.getElementById("app").innerHTML = "howdy"
+
+class NoteController {
+    constructor() {
+        this.noteList = new NoteList()
+        this.noteList.createNote("Favourite drink: seltzer")
+        this.noteList.notesList.showList()
+    }
+}
